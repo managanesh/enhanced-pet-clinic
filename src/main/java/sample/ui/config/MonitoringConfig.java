ae18cca639a7361e0147aad76af6c9db6f9182d5
Nicolas Frankel <nicolas@frankel.ch>
2015-02-15 21:15:36 +0100

    Add Dropwizard metrics

---
+++ src/main/java/sample/ui/config/MonitoringConfig.java
@@ -0,0 +1,21 @@
+package sample.ui.config;
+
+import com.codahale.metrics.JmxReporter;
+import com.codahale.metrics.MetricRegistry;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MonitoringConfig {
+
+    @Autowired
+    private MetricRegistry registry;
+
+    @Bean
+    public JmxReporter jmxReporter() {
+        JmxReporter reporter = JmxReporter.forRegistry(registry).build();
+        reporter.start();
+        return reporter;
+    }
+}
