13410b4414ea0d245a854f119dfbd7aedcaacd48
Nicolas Frankel <nicolas@frankel.ch>
2015-02-15 21:16:05 +0100

    Add Graphite reporter

--- src/main/java/sample/ui/config/MonitoringConfig.java
+++ src/main/java/sample/ui/config/MonitoringConfig.java
@@ -2,15 +2,30 @@ package sample.ui.config;
 
 import com.codahale.metrics.JmxReporter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class MonitoringConfig {
 
     @Autowired
     private MetricRegistry registry;
+
+    @Bean
+    public GraphiteReporter graphiteReporter() {
+        Graphite graphite = new Graphite(
+            new InetSocketAddress("localhost", 2003));
+        GraphiteReporter reporter = GraphiteReporter.forRegistry(registry)
+            .prefixedWith("boot").build(graphite);
+        reporter.start(500, TimeUnit.MILLISECONDS);
+        return reporter;
+    }
 
     @Bean
     public JmxReporter jmxReporter() {
