d9030d594837da243bdac8259140fb1165ca975c
Nicolas Frankel <nicolas@frankel.ch>
2015-07-09 16:34:43 +0200

    Add Graphite reporter

--- src/main/java/sample/ui/config/MonitoringConfig.java
+++ src/main/java/sample/ui/config/MonitoringConfig.java
@@ -1,5 +1,6 @@
 package sample.ui.config;
 
+import java.util.concurrent.TimeUnit;
 import javax.inject.Named;
 import org.springframework.boot.actuate.autoconfigure.ExportMetricWriter;
 import org.springframework.boot.actuate.metrics.jmx.JmxMetricWriter;
@@ -7,6 +8,9 @@ import org.springframework.boot.actuate.metrics.writer.MetricWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jmx.export.MBeanExporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.graphite.Graphite;
+import com.codahale.metrics.graphite.GraphiteReporter;
 
 @Configuration
 public class MonitoringConfig {
@@ -15,5 +19,13 @@ public class MonitoringConfig {
     @ExportMetricWriter
     MetricWriter metricWriter(@Named("endpointMBeanExporter") MBeanExporter exporter) {
         return new JmxMetricWriter(exporter);
+    }
+
+    @Bean
+    GraphiteReporter graphiteExporter(MetricRegistry metricRegistry) {
+        Graphite graphite = new Graphite("localhost", 2003);
+        GraphiteReporter reporter = GraphiteReporter.forRegistry(metricRegistry).prefixedWith("boot").build(graphite);
+        reporter.start(500, TimeUnit.MILLISECONDS);
+        return reporter;
     }
 }
